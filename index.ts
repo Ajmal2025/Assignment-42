61a10a1825cf10511681f0d9185d31adbfbba4d0
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 23:05:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+
+
+
+// 42.	Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+
+function makeGreat(magicianNames: string[]): string[] {
+    const greatMagicians: string[] = magicianNames.map(name => name + " the Great");
+    return greatMagicians;
+}
+
+function showMagicians(magicianNames: string[]): void {
+    for (const name of magicianNames) {
+        console.log(name);
+    }
+}
+
+const magicians: string[] = ["Samri", "Goga", "Chintu", "Black"];
+const greatMagicians = makeGreat(magicians);
+
+console.log("Original Magicians:");
+showMagicians(magicians);
+
+console.log("\nGreat Magicians:");
+showMagicians(greatMagicians);
